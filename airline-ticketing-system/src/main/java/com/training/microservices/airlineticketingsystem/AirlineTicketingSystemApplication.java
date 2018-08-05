54ab747dbfc38405a9139e501f36756a53001f71
TejaswiniM30 <tejaswinim3003@gmail.com>
2018-08-05 22:20:41 +0530

    spring-boot-mongo

---
+++ airline-ticketing-system/src/main/java/com/training/microservices/airlineticketingsystem/AirlineTicketingSystemApplication.java
@@ -0,0 +1,12 @@
+package com.training.microservices.airlineticketingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineTicketingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineTicketingSystemApplication.class, args);
+	}
+}
